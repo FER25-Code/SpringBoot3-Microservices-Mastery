bcae870b61690e83cdd8e7084b5acea94fe69ae9
FER25-Code <Dia.fergani@univ-constantine2.dz>
2023-05-12 23:36:04 +0100

    [back] create new projects

---
+++ gateway/src/test/java/com/fer/code25/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer.code25.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
