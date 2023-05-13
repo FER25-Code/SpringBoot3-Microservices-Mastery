5a8b82a3f21e95d809d9e47d13d3121249c63d6e
FER25-Code <Dia.fergani@univ-constantine2.dz>
2023-05-13 19:15:52 +0100

    [back-end] Develop a microservice-based architecture project using Spring Boot and Eureka for seamless service discovery and registration. This ensures scalability, fault tolerance, and independent deployment capabilities.

--- student/src/main/java/com/fer/code25/student/StudentApplication.java
+++ student/src/main/java/com/fer/code25/student/StudentApplication.java
@@ -2,7 +2,7 @@ package com.fer.code25.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class StudentApplication {
